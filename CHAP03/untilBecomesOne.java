4cd3431625acb5f80090da579ec3c5aae53e3989
dndqlszzz <mandarin1999@naver.com>
2025-01-03 16:19:40 +0900

    1이될때까지

--- CHAP03/untilBecomesOne.java
+++ CHAP03/untilBecomesOne.java
@@ -10,9 +10,14 @@ public class untilBecomesOne {
         int N = Integer.parseInt(input[0]);
         int K = Integer.parseInt(input[1]);
 
+        // N을 K로 나눈 횟수
         int numberOfDivide = 0;
+        // 나눠지지 않아서 1을 뺀 횟수 = N을 K로 나눈 나머지
         int sol = N%K;
+
+        // 나눠지는 수의 횟수
         while(true){
+            // 1이 될 때 까지 나눈 후, 나눈 횟수 + 1을 뺀 횟수 더함
             if(N==1){
                 sol += numberOfDivide;
                 break;
