fb3135806563773394afcd5dc04d1e7dfcfb926d
dndqlszzz <mandarin1999@naver.com>
2025-01-07 16:20:21 +0900

    time

--- CHAP04/time.java
+++ CHAP04/time.java
@@ -1,25 +1,25 @@
 package CHAP04;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class time {
     public static void main(String[] args) throws IOException{
-        final int MAXMINUTE = 59;
-        final int MAXSECOND = 59;
+        int sol = 0;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String N = br.readLine();
-        if(N.length()==1) {
-            N = "0" + N;
-        }
-        String[] numbers = N.split("");
 
         for(int i=0;i<=Integer.parseInt(N);i++){
-            if(Arrays.stream(numbers).anyMatch("3"))
+            for(int j=0; j<60; j++){
+                for(int k=0; k < 60; k++){
+                    if((i%10)==3) sol ++;
+                    else if ((j%10)==3 || ((j%100)>=30 && (j%100)<40)) sol ++;
+                    else if ((k%10)==3 || ((k%100)>=30 && (k%100)<40)) sol ++;
+                }
+            }
         }
-
-
+        System.out.println(sol);
     }
 }
