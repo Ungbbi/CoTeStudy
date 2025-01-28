9004e141c3d45d2a3a317f24d1588cebae8d1239
dndqlszzz <mandarin1999@naver.com>
2025-01-28 15:13:03 +0900

    makingRiceCake

--- CHAP07/makingRiceCake.java
+++ CHAP07/makingRiceCake.java
@@ -6,21 +6,36 @@ import java.io.BufferedReader;
 import java.util.Arrays;
 
 public class makingRiceCake {
-    public static Integer binarySearch(int[] riceCakes, int target, int start, int end){
-        if(start>=end) return riceCakes[start-1];
-        int mid = (start+end)/2;
-        int sum = 0;
-        for(int i=mid+1; i<=end;i++){
-            sum += riceCakes[i] - riceCakes[mid];
+
+    public static Integer binarySearch(int[] riceCakes, int target, int start, int end) {
+        if (start >= end) return riceCakes[start - 1];
+        int mid;
+        int sum;
+        int temp;
+        while (start < end) {
+            sum = 0;
+            mid = (start + end) / 2;
+            for (int i = 0; i < riceCakes.length; i++) {
+                temp = riceCakes[i] - mid;
+                if (temp>0){
+                    sum+=temp;
+                }
+            }
+            if (sum == target) {
+                return mid;
+            }
+            // 기준이 요구치보다 값이 큼 -> 기준을 크게 ( 더 길게 잘라서 총 합을 줄여야함 )
+            else if (sum > target) {
+                start = mid + 1;
+            }
+            // 기준이 요구치보다 값이 작음 -> 기준을 작게 ( 더 짧게 잘라서 총 합을 늘려야함 )
+            else {
+                end = mid - 1;
+            }
         }
-        if(sum==target) return riceCakes[mid];
-        else if(sum>target){
-            return binarySearch(riceCakes, target, mid+1, end);
-        }
-        else{
-            return binarySearch(riceCakes, target, start, mid-1);
-        }
+        return null;
     }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputs = br.readLine().split(" ");
@@ -32,8 +47,8 @@ public class makingRiceCake {
             inputRiceCakes[i] = Integer.parseInt(inputs[i]);
         }
 
-        Arrays.sort(riceCakes);
-        Integer sol = binarySearch(riceCakes, M,0,riceCakes.length-1);
+        Arrays.sort(inputRiceCakes);
+        Integer sol = binarySearch(inputRiceCakes, M,inputRiceCakes[0],inputRiceCakes[inputRiceCakes.length-1]);
         System.out.println(sol);
     }
 }
