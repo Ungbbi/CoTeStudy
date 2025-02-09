903e42201b9729abd0506eb22b8ab958e034857c
Ungbbi <mandarin1999@naver.com>
2025-02-09 19:18:48 +0900

    Add Comment

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,10 +1,4 @@
-/*
-1. 정렬
-2. 앞에 값부터 뽑고 인덱스를 통해 뽑은 원소 앞에 몇개가 있는지 확인 
-*/
 import java.util.Arrays;
-import java.util.Collections;
-
 
 class Solution {
     
@@ -23,3 +17,18 @@ class Solution {
         return result;
     }
 }
+
+/*
+정렬 후 최댓값을 가지고 반복문에 활용.
+1. while(i > citations[x]) x++;
+=> citations의 value값이 인용횟수이고, i는 인덱스이자 인용횟수이다.
+반복문 진행 중 처음으로 x++로 인해 i <= citations[x]가 될 때, 
+              그 이후는 모두 i보다 인용횟수가 많다는 것이다.
+그 때 "논문이 h편 이상이고," 를 체크하는 것이  (i <= citations.length - x)
+왜 citations.length - x 인가 ? 
+-> x값 이전 인덱스들은 조건을 만족시키지 못한다. 하지만 x이상의 인덱스들이 조건들을 포함하므로
+조건을 만족하는 논문 개수를 구하는 식.
+i <= (citations.length -x) 는?
+-> 문제에서 h번 이상 인용된 논문이 h편 이상이어야 하므로.
+"h의 최댓값"을 구하는 것이 (i > result)
+*/
