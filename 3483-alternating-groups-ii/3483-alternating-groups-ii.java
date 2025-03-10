c8318a002bc2e41f78ca1447f2515eccf7e006d1
Ungbbi <mandarin1999@naver.com>
2025-03-10 17:23:58 +0900

    Time: 3 ms (86.91%), Space: 56.3 MB (95.82%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.java
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -1,29 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        int ruletIndex = colors.length;
-        int prevColor = colors[0];
-        int groupCnt = 0;
-        int tileCnt = 1;
-        int right = 0 ;
-        int left = 0;
-        while(left<colors.length){
-            if(prevColor != colors[right%colors.length]){
-                prevColor = colors[right%colors.length];
-                tileCnt++;
-                if(tileCnt==k){
-                    groupCnt++;
-                    tileCnt--;
-                    left++;
-                }
-            }
-            else {
-                left = right;
-                tileCnt = 1;
-            }
-            right ++;
+        
+        int result = 0;
+
+        int[] copy = new int[colors.length - 1 + k];
+
+        for(int i= 0 ; i< colors.length; i++){
+            copy[i] = colors[i];
         }
-        return groupCnt;
+
+        for(int i = colors.length; i < copy.length; i++){
+            copy[i] = colors[i - colors.length];
+        }
+        
+        int l = 0;
+        int r = 0;
+
+        while(r + 1< copy.length ){
+            if(copy[r] != copy[r + 1]){
+                r++;
+            }else{
+                r = r + 1;
+                l = r;
+            }
+
+            if(r - l + 1== k) {
+                result++;
+                l++;
+            }
+
+        }
+        
+        return result;
     }
 }
