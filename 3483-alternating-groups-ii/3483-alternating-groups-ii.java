51b533fa2b88dd231bc185cfc04c4e093ed4d9fe
Ungbbi <mandarin1999@naver.com>
2025-03-10 17:22:38 +0900

    Time: 3 ms (86.91%), Space: 62.7 MB (73.54%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int ruletIndex = colors.length;
+        int prevColor = colors[0];
+        int groupCnt = 0;
+        int tileCnt = 1;
+        int right = 0 ;
+        int left = 0;
+        while(left<colors.length){
+            if(prevColor != colors[right%colors.length]){
+                prevColor = colors[right%colors.length];
+                tileCnt++;
+                if(tileCnt==k){
+                    groupCnt++;
+                    tileCnt--;
+                    left++;
+                }
+            }
+            else {
+                left = right;
+                tileCnt = 1;
+            }
+            right ++;
+        }
+        return groupCnt;
+    }
+}
