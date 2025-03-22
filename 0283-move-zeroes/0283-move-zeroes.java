3050f6eb49b58b9cced783c4222e97261d4ea279
Ungbbi <mandarin1999@naver.com>
2025-03-22 15:32:41 +0900

    Time: 1 ms (99.47%), Space: 45.9 MB (64.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,28 +1,18 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        // 1. 0을 뒤로 다 옮기고 나서 정렬 -> x 순서를 유지시켜야함 
-        int l = 0;
-        int r = 0;
-        
-        // Step 1. Move Zeors to the end.
-        while(r<nums.length){
-            if(l==r && nums[l] != 0){
-                l++;
-                r++;
-                continue;
-            }
-            if(nums[r]==0){
-                r++;
-                continue;            
-            }
-            if(nums[l]==0){
-                nums[l] = nums[r];
-                nums[r] = 0;        
-                l++;
-            }
+    public void moveZeroes(int[] array) {        
+        int size = array.length;
+        int k = 0;
 
+        for(int i = 0; i < size; i++) {
+            if (array[i] != 0) {
+                array[k] = array[i];
+                k++;
+            }
         }
 
-        System.out.println(Arrays.toString(nums));
-    }
+        for(int i = k; i < size; i++) {
+            array[i] = 0;
+        }
+
+    } 
 }
