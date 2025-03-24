c00c579cc314ae5351ad381c18830eb83e1be5b0
Ungbbi <mandarin1999@naver.com>
2025-03-24 22:13:44 +0900

    [Silver II] Title: 나무 자르기, Time: 1024 ms, Memory: 119596 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+//  좌표 정렬
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N,M;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 길이 H보다 작으면 냅두고 크면 자르고 남은 나머지를 가져간다.
+        st = new StringTokenizer(br.readLine()," ");
+        int[] trees = new int[N];
+        for(int i=0; i<N; i++){
+            trees[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(trees);
+
+        int lo = 0;
+        int hi = trees[N-1];
+        long result = 0;
+
+        while(lo <= hi){
+            int mid = (hi+lo)/2;
+            long total = 0;
+
+            for(int i=0; i<N; i++){
+                long remain = trees[i] - mid;
+                if(remain<=0) continue;
+                total += remain;
+            }
+
+            // 가져가려는 길이보다 부족 -> 너무 크게 자름 -> 자르는 길이 축소
+            if(total<M){
+                hi = mid-1;
+            }
+            // 가져가려는 길이보다 많음 -> 너무 작게 자름 -> 자르는 길이 확장
+            if(total>=M){
+                lo = mid+1;
+                result = mid;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
